664c8be3702d78183256af663e186c70145aefd6
sniper <81185001@163.com>
2020-05-11 11:46:19 +0800

    exc 中各个异常的__init__ 参数data设置默为空字符串

--- app/exc/__init__.py
+++ app/exc/__init__.py
@@ -3,7 +3,7 @@
 
 
 class YcmsError(Exception):
-    def __init__(self, message, data, logit=False):
+    def __init__(self, message, data='', logit=False):
         self.message = message
         self.data = data
 
@@ -18,7 +18,7 @@ class YcmsError(Exception):
 class YcmsTableNotExistsError(YcmsError):
     """ 待操作的数据表不存在
     """
-    def __init__(self, message, data, logit=False):
+    def __init__(self, message, data='', logit=False):
         message = message or '待操作的数据表不存在: ' + str(data)
         super().__init__(message, data, logit)
 
@@ -26,18 +26,19 @@ class YcmsTableNotExistsError(YcmsError):
 class YcmsSqlParseError(YcmsError):
     """ 解析sql失败
     """
-    def __init__(self, message, data, logit=False):
+    def __init__(self, message, data='', logit=False):
         message = message or '解析sql失败 ' + str(data)
         super().__init__(message, data, logit)
 
 
 class YcmsSqlOperatorError(YcmsSqlParseError):
-    def __init__(self, message, data, logit=False):
+    def __init__(self, message, data='', logit=False):
         message = message or '非法sql操作符 ' + str(data)
         super().__init__(message, data, logit)
 
 
 class YcmsSqlConditionParseError(YcmsSqlParseError):
-    def __init__(self, message, data, logit=False):
+    def __init__(self, message, data='', logit=False):
         message = message or '非法sql条件 ' + str(data)
         super().__init__(message, data, logit)
+
