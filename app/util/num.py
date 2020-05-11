3fc3c4256c75e95c60016844086cac42f989ac78
sniper <81185001@163.com>
2020-05-11 17:36:55 +0800

    新增： 工具函数 num.py 新增 float相关 及int_unsigned’

--- app/util/num.py
+++ app/util/num.py
@@ -1,10 +1,19 @@
 """ 数字相关操作
 """
+### 整形
 def parse_int_or_zero(arg):
     try:
         return int(str(arg))
     except ValueError as e:
         return 0
+
+
+def parse_int_or_zero_unsigned(arg):
+    try:
+        return int(str(arg)) > 0 or 0
+    except ValueError as e:
+        return 0
+
 
 def parse_int_or_false(arg):
     try:
@@ -12,8 +21,38 @@ def parse_int_or_false(arg):
     except ValueError as e:
         return False
 
+
 def parse_int_or_none(arg):
     try:
         return int(str(arg))
+    except ValueError as e:
+        return None
+
+### float
+
+def parse_float_or_zero(arg):
+    try:
+        return float(str(arg))
+    except ValueError as e:
+        return 0
+
+
+def parse_float_or_zero_unsigned(arg):
+    try:
+        return float(str(arg)) > 0 or 0
+    except ValueError as e:
+        return 0
+
+
+def parse_float_or_false(arg):
+    try:
+        return float(str(arg))
+    except ValueError as e:
+        return False
+
+
+def parse_float_or_none(arg):
+    try:
+        return float(str(arg))
     except ValueError as e:
         return None
