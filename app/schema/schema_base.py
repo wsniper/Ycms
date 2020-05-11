eeca7a4b6830004773aa9a7e42324c14aa1c7250
sniper <81185001@163.com>
2020-05-11 15:26:03 +0800

    修改schema user add time / update time 自动设置时间 default=lambda ...‘

--- app/schema/schema_base.py
+++ app/schema/schema_base.py
@@ -1,3 +1,5 @@
+import time
+
 from sqlalchemy import Column, Integer, String, Text, Index
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -7,8 +9,12 @@ class BaseSchemaMixin:
     id = Column('id', Integer, primary_key=True, autoincrement=True, nullable=False)  
     # status: 0->已删除 1->禁用 99->正常
     status = Column('status', Integer, nullable=False, server_default='99')  
-    add_time = Column('add_time', Integer, nullable=False, server_default='0')  
-    last_update_time = Column('last_update_time', Integer, nullable=False, server_default='0')  
+    add_time = Column('add_time', Integer, nullable=False, 
+                              default=lambda x: str(int(time.time())),
+                              onupdate=lambda x: str(int(time.time())), server_default='0')  
+    last_update_time = Column('last_update_time', Integer, nullable=False, 
+                              default=lambda x: str(int(time.time())),
+                              onupdate=lambda x: str(int(time.time())), server_default='0')  
 
     def __repr__(self):
         return '<{table_name}{fields}>'\
